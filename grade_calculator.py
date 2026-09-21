7b66a6a173ae696882a35817eb077fd545d393c9
evasher <evashermburu@gmail.com>
2026-09-21 08:42:14 +0300

    Update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -5,3 +5,15 @@ english=float(input("Enter your english mark:"))
 physics=float(input("Enter your physics mark:"))
 average=(maths+english+physics)/3
 print("Your average is:",average)
+if average >=80:
+  grade="A"
+elif average >=70:
+  grade="B"
+elif average >=60:
+  grade="C"
+elif average >=50:
+  grade="D"
+else:
+  grade='"E"
+
+print(Your grade is:", grade)
